8281b26285fffac77e295d1aef077dc75526ee92
Satheesh Valluru <satheesh.valluru@mulesoft.com>
2019-02-26 14:30:48 +0530

    Complete Trail

---
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -0,0 +1,48 @@
+import { LightningElement, track, wire } from 'lwc';
+import { NavigationMixin, CurrentPageReference } from 'lightning/navigation';
+import { fireEvent } from 'c/pubsub';
+import { loadStyle } from 'lightning/platformResourceLoader';
+
+import ursusResources from '@salesforce/resourceUrl/ursus_park';
+/** BearController.searchBears(searchTerm) Apex method */
+import searchBears from '@salesforce/apex/BearController.searchBears';
+export default class BearListNav extends NavigationMixin(LightningElement) {
+	@track searchTerm = '';
+    @track bears;
+    @wire(CurrentPageReference) pageRef;
+    @wire(searchBears, {searchTerm: '$searchTerm'})
+    loadBears(result) {
+        this.bears = result;
+        if (result.data) {
+            fireEvent(this.pageRef, 'bearListUpdate', result.data);
+        }
+    }
+	connectedCallback() {
+		loadStyle(this, ursusResources + '/style.css');
+	}
+	handleSearchTermChange(event) {
+		// Debouncing this method: do not update the reactive property as
+		// long as this function is being called within a delay of 300 ms.
+		// This is to avoid a very large number of Apex method calls.
+		window.clearTimeout(this.delayTimeout);
+		const searchTerm = event.target.value;
+		// eslint-disable-next-line @lwc/lwc/no-async-operation
+		this.delayTimeout = setTimeout(() => {
+			this.searchTerm = searchTerm;
+		}, 300);
+	}
+	get hasResults() {
+		return (this.bears.data.length > 0);
+	}
+	handleBearView(event) {
+		// Navigate to bear record page
+		this[NavigationMixin.Navigate]({
+			type: 'standard__recordPage',
+			attributes: {
+				recordId: event.target.bear.Id,
+				objectApiName: 'Bear__c',
+				actionName: 'view',
+			},
+		});
+	}
+}
---
+++ force-app/main/default/lwc/bearLocation/bearLocation.js
@@ -0,0 +1,32 @@
+import { LightningElement, api, track, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+const fields = [
+	'Bear__c.Name',
+	'Bear__c.Location__Latitude__s',
+	'Bear__c.Location__Longitude__s'
+];
+export default class BearLocation extends LightningElement {
+	@api recordId;
+	@track name;
+	@track mapMarkers = [];
+	@wire(getRecord, { recordId: '$recordId', fields })
+	loadBear({ error, data }) {
+		if (error) {
+			// TODO: handle error
+		} else if (data) {
+			// Get Bear data
+			this.name = data.fields.Name.value;
+			const Latitude = data.fields.Location__Latitude__s.value;
+			const Longitude = data.fields.Location__Longitude__s.value;
+			// Transform bear data into map markers
+			this.mapMarkers = [{
+				location: { Latitude, Longitude },
+				title: this.name,
+				description: `Coords: ${Latitude}, ${Longitude}`
+			}];
+		}
+	}
+	get cardTitle() {
+		return (this.name) ? `${this.name}'s location` : 'Bear location';
+	}
+}
